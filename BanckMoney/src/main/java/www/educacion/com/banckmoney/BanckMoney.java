cc4cd5e1059b1ca9ee3f733b36d107206388785e
KarenMarcela <kmbayonam@ufpso.edu.co>
2024-04-01 21:30:36 -0500

    Ejercicio del banco (1)

---
+++ BanckMoney/src/main/java/www/educacion/com/banckmoney/BanckMoney.java
@@ -0,0 +1,23 @@
+
+
+package www.educacion.com.banckmoney;
+
+import www.educacion.com.banckmoney.people.User;
+
+public class BanckMoney {
+
+    public static void main(String[] args) {
+        
+        User user1= new User("Anamaría");
+        User user2= new User("Karen", 50000);
+        
+        user1.whithdrawMoney(20_000);
+        user2.consignMoney(100_000);
+        
+        System.out.println("La cantidad total asignada es $"+user2.getMoney());
+        
+        
+        
+        
+    }
+}
---
+++ BanckMoney/src/main/java/www/educacion/com/banckmoney/people/User.java
@@ -0,0 +1,55 @@
+
+package www.educacion.com.banckmoney.people;
+
+
+public class User {
+    private String name;
+    private double money;
+
+    public User(String name, double money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public User(String name) {
+        this.name = name;
+        this.money=0;
+    }
+
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    
+    public void consignMoney(double qty){
+        if(this.money < 0){
+            System.out.println("No se puede consignar");
+            return;
+        }
+        this.money += qty;
+        System.out.println("Consignación exitosa");
+    }
+    public void whithdrawMoney(double qty){
+        if(qty > this.money){
+            System.out.println("No se puede retirar, dinero insuficiente");
+            return;
+        }
+        this.money -= qty;
+        System.out.println("Retiro exitoso");
+    }
+}
