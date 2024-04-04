681bb36249758dd0b4570f029ac31f1e95f07791
KarenMarcela <kmbayonam@ufpso.edu.co>
2024-04-03 22:29:21 -0500

    Ejercicio 2, completado

--- Characters/src/main/java/www/educacion/com/characters/Characters.java
+++ Characters/src/main/java/www/educacion/com/characters/Characters.java
@@ -1,9 +1,52 @@
 
 package www.educacion.com.characters;
+import java.util.Scanner;
+import www.educacion.com.characters.people.People;
+
 
 public class Characters {
 
     public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Ingrese su nombre : ");
+        String name=scanner.next();
+        
+        System.out.print("Ingrese su edad : ");
+        int age=scanner.nextInt();
+        
+        System.out.print("Ingrese su sexo (F o M): ");
+        char sex=scanner.next().charAt(0);
+        
+        System.out.print("Ingrese su peso : ");
+        double weight= scanner.nextDouble();
+        
+        System.out.print("Ingrese su altura(el decimal se indica con una coma) : ");
+        double height=scanner.nextDouble();
+        
+        People person1= new People (name, age, sex,weight,height);
+        People person2= new People (name, age, sex);
+        People person3= new People ();
+        
+        
+        
+        System.out.println("************************************");
+        System.out.println("Persona 1: ");
+        showResults(person1);
+                
+        System.out.println("************************************");
+        System.out.println("Persona 2: ");
+        showResults(person2);
+        System.out.println("************************************");
+        System.out.println("Persona 3: ");
+        showResults(person3);
+        
+        
         
     }
+    public static void showResults(People people){
+            System.out.print("Su IMC es: "+people.calcularIMC());
+            System.out.print("¿Es mayor de edad? :"+people.biggerMinor());
+            System.out.println("Info personal: "+people.toString());
+        }
 }
+
--- Characters/src/main/java/www/educacion/com/characters/people/People.java
+++ Characters/src/main/java/www/educacion/com/characters/people/People.java
@@ -1,11 +1,13 @@
 
 package www.educacion.com.characters.people;
 
+import java.util.Random;
+
 
 public class People {
     private String name;
     private int age;
-    private int dni;
+    private String dni;
     private char sex;
     private static final char sex_flaw='F';
     private double weight;
@@ -17,7 +19,7 @@ public class People {
 
     // Constructor sin parametros, dando valores por defecto
     public People() {
-        this.name = "";
+        this.name = " ";
         this.age = 0;
         this.sex = sex_flaw;
         this.weight = 0;
@@ -39,20 +41,80 @@ public class People {
         this.weight = weight;
         this.height = height;
     }
-    public void calcularIMC(){
-        
-        
+
+    //Calulcar imc ( validación)
+    public double calcularIMC(){
         double properWeight= (this.weight/(this.height*this.height));
-        System.out.println("Bien = 1__ Estas por debajo del peso ideal = 0__ Mal = -1");
+        System.out.println("Bien= 1   Regular= 0  Mal= -1");
         if(properWeight < 20){
-            System.out.println(mal);
+            return mal;
         }
         else if(properWeight >= 20 && properWeight <= 25){
-            System.out.println(regular);
+            return regular;
         }
         else{
-            System.out.println(bien);
+            return bien;
         }
+    }
+    
+    //metodo para comprobar si es mayor de edad
+    public boolean biggerMinor(){
+        return age >= 18;
+    }
+    
+    //comprobar el sexo(sino es correcto, por defecto será H)
+    public char checkSex(char resultSex){
+        if(sex == this.sex){
+            resultSex= this.sex;
+        }
+        else{
+            resultSex= 'H';
+        }
+        return resultSex;
+    }
+    
+    //devolver toda la información del objeto
+    public String toString(){
+        return ("Nombre : "+name+" Edad: "+age+" DNI : "+dni+" Peso: "+weight+" Altura: "+height);
+    }
+    
+    //generar un numero para el DNI aleatorio
+    public void triggerDni(){
+        Random random= new Random();
+        StringBuilder dniBuilder = new StringBuilder();
+        //Genera los primeros 8 digitos del dni
+        for(int i=0;i<8 ;i++){
+            dniBuilder.append(random.nextInt(10));
+        }
+        int dniNumber = Integer.parseInt(dniBuilder.toString());
+        char dniLetter= calcularLetraDNI(dniNumber);//Calcula la letra correspondiente
+        this.dni = dniNumber + String.valueOf(dniLetter);
+    }
+    //Metodo privado para calcular la letra del dni
+    private char calcularLetraDNI(int dniNumber) {
+        String letters= "TRWAGMYFPDXBNJZSQVHLCKE";
+        int rest= dniNumber % 23;
+        return letters.charAt(rest);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
     
     
